1b0e25aa2410adef5c189508bc63defd83b4f4a6
James <jamesandrewpike@gmail.com>
2024-04-15 23:01:53 -0400

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 'use client'
 
-import BrandBanner from "@/components/brand-banner";
-import BasicFooter from "@/components/footer";
-import { LocationProvider } from "@/components/locationcontext";
-import Pricing from "@/components/pricing";
-import { title } from "@/components/primitives";
-import Video from "@/components/video";
-
 export default function MenuPage() {
     return (
         <div className="">
@@ -16,17 +9,7 @@ export default function MenuPage() {
            
             <section className="mx-auto w-full lg:px-5 ">
 
-                
-            {/* <video
-                        src={"/video/almonte1.mp4"}
-                        loop
-                        autoPlay
-                        muted
-                        style={{ opacity: 0.75, height: '20vh' }}
-                        className='object-cover w-full h-full lg:px-40 '
-                    >
-                    </video> */}
-            {/* <BasicFooter/> */}
+
 
             <div className="h-screen"></div>
     </section>
