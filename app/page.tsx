dfa692768b0b855c3ac742edbf4fdecde89cc4c9
James <jamesandrewpike@gmail.com>
2024-04-16 14:20:36 -0400

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { title } from "@/components/primitives";
 
 export default function HomePage() {
     return (
-        <div className="flex items-center justify-center pt-8">
+        <div className="flex items-center justify-center pt-8 pb-40">
             <h1 className={title()} style={{ textAlign: 'center' }}>
                 Home Page
             </h1>
