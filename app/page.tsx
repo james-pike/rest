a19a4919677d89a8e45bcf658ac00a83e3e7090e
James <jamesandrewpike@gmail.com>
2024-04-17 16:56:15 -0400

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { title } from "@/components/primitives";
 
 export default function HomePage() {
 	return (
+		<div>
 		<div style={{ position: 'relative' }}>
 		<img
 		  className="w-full h-40 object-cover border-b"
@@ -25,5 +26,45 @@ export default function HomePage() {
 		  Menu 
 		</h1>
 	  </div>
+
+
+
+	  <div style={{ position: 'relative' }}>
+		<img
+		  className="w-full h-40 object-cover border-b"
+		  style={{ opacity: 0.75, height: '20vh' }}
+		  src="./images/a1.jpg"
+		  alt="Background Image"
+		/>
+		<h1
+		  className={title()}
+		  style={{
+			
+			position: 'absolute',
+			top: '50%', // Position the h1 in the middle vertically
+			left: '25%', // Position the h1 in the middle horizontally
+			transform: 'translate(-50%, -50%)', // Center the h1
+			color: 'white', // Text color
+			fontSize: '3rem', // Font size
+			zIndex: 10, // Ensure it's above the image
+		  }}
+		>
+		  Locations 
+		</h1>
+
+		
+
+
+	  </div>
+
+
+
+	  
+	  </div>
+
+
+
+
+	  
 	);
 }
