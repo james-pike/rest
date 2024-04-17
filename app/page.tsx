a1861ba731184a4c7a9c262d3b6e279d98c2ed20
James <jamesandrewpike@gmail.com>
2024-04-17 17:23:56 -0400

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default function HomePage() {
 				<img
 					className="w-full h-40 object-cover border-b"
 					style={{ opacity: 0.75, height: '20vh' }}
-					src="./images/events.jpg"
+					src="./images/events3.jpg"
 					alt="Background Image"
 				/>
 				<h1
@@ -77,6 +77,32 @@ export default function HomePage() {
 					}}
 				>
 					Events
+				</h1>
+			</div>
+
+
+
+			<div style={{ position: 'relative' }}>
+				<img
+					className="w-full h-40 object-cover border-b"
+					style={{ opacity: 0.75, height: '20vh' }}
+					src="./images/c1.jpg"
+					alt="Background Image"
+				/>
+				<h1
+					className={title()}
+					style={{
+
+						position: 'absolute',
+						top: '50%', // Position the h1 in the middle vertically
+						left: '25%', // Position the h1 in the middle horizontally
+						transform: 'translate(-50%, -50%)', // Center the h1
+						color: 'white', // Text color
+						fontSize: '2.5rem', // Font size
+						zIndex: 10, // Ensure it's above the image
+					}}
+				>
+					Catering
 				</h1>
 			</div>
 
