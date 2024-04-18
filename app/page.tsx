958871436ce6ade72d5c2debfcc81e786f28d4dd
James <jamesandrewpike@gmail.com>
2024-04-18 18:26:02 -0400

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { title } from "@/components/primitives";
 export default function HomePage() {
 	return (
 		<div>
-
+<div style={{ position: 'relative' }}>
 <video
 					className="w-full h-40 object-cover border-b-gray-700"
 					style={{ opacity: 0.75, height: '28vh' }}
@@ -13,6 +13,29 @@ export default function HomePage() {
 					poster="./images/a6.png"
 					loop
 				/>
+				<h1
+					className={title()}
+					style={{
+
+						position: 'absolute',
+						top: '50%', // Position the h1 in the middle vertically
+						left: '25%', // Position the h1 in the middle horizontally
+						transform: 'translate(-50%, -50%)', // Center the h1
+						color: 'white', // Text color
+						fontSize: '2rem', // Font size
+						zIndex: 10, // Ensure it's above the image
+					}}
+				>
+					Joe's Italian Kitchen
+				</h1>
+</div>
+
+
+
+
+
+
+
 			 <div className="border-8 border-black"></div>
 
 
