8beaf04267dcf5d73027cc9124305085e3aee157
James <jamesandrewpike@gmail.com>
2024-04-20 14:34:11 -0400

    commit

--- app/locations/layout.tsx
+++ app/locations/layout.tsx
@@ -1,4 +1,6 @@
 import BasicFooter from "@/components/footer";
+import { CarouselDemo } from "@/components/location-carousel";
+import { Carousel } from "@/components/ui/carousel";
 import { AcmeIcon } from "@/social";
 import ThemeSwitch from "@/theme-switch";
 import Link from "next/link";
@@ -11,8 +13,20 @@ export default function AboutLayout({
 	return (
 
 		<div>
-			<img className="w-full h-40 object-cover border-b" src="./images/p1.jpg" />
+			<img 
+      	style={{ opacity: 0.75, height: '20vh' }} 
+      className="w-full  object-cover" src="./images/p1.jpg" />
       <img className="w-full h-80 object-cover border-12 border-black opacity-50" src="./images/w2.jpg" />
+
+      <img 
+      	style={{ opacity: 0.75, height: '33vh' }} 
+      className="w-full  object-cover border-black border-8" src="./images/p1.jpg" />
+
+<img 
+      	style={{ opacity: 0.75, height: '33vh' }} 
+      className="w-full  object-cover border-8 border-black" src="./images/a2.jpg" />
+
+      <CarouselDemo/>
 <BasicFooter />
 		
 	
