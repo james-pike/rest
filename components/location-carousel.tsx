6bba646206550014cbe623d45ec32d3eb6ce769a
James <jamesandrewpike@gmail.com>
2024-04-20 14:41:59 -0400

    commit

--- components/location-carousel.tsx
+++ components/location-carousel.tsx
@@ -23,8 +23,8 @@ export function WellingtonCarousel() {
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
+      {/* <CarouselPrevious /> */}
+      {/* <CarouselNext /> */}
     </Carousel>
   )
 }
